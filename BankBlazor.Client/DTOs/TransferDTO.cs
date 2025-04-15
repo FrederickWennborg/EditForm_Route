e42f33c13a2fd3ef35b74a93de72c88ec5b43e0f
FrederickWennborg <frederick.wennborg@systementor.se>
2025-04-15 20:12:25 +0200

    Add project files.

---
+++ BankBlazor.Client/DTOs/TransferDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankBlazor.Client.DTOs
+{
+    public class TransferDTO
+    {
+        public int FromAccountId { get; set; }
+
+        [Required]
+        public int ToAccountId { get; set; }
+
+        public decimal Amount { get; set; }
+
+    }
+}
